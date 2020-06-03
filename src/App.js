7f05a1c01dcd1cd7b2e1732cd6b3ff7da2216124
sadewole <samueladewole15@gmail.com>
2020-06-03 19:10:59 +0100

    added necessary dependencies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { LandingPage } from './components/pages';
 import './App.css';
+import NavBar from './components/layouts/NavBar';
 
 function App() {
   return (
     <div className='App'>
+      <NavBar />
       <LandingPage />
     </div>
   );
--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar bg='light' variant='light' fixed='top'>
+        <Navbar.Brand href='#home'>BuckMed</Navbar.Brand>
+        <Nav className='mr-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#features'>Features</Nav.Link>
+          <Nav.Link href='#pricing'>Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+    </div>
+  );
 };
 
 export default NavBar;
