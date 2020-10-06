705d5650853f9cbc82ff9442e435dd003eaca444
sadewole <samueladewole15@gmail.com>
2020-10-06 08:55:52 +0100

    updated icons with iconify

--- src/layouts/DoctorLayout/sidebar/NavItem.js
+++ src/layouts/DoctorLayout/sidebar/NavItem.js
@@ -3,72 +3,11 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core';
+import { Icon } from '@iconify/react';
 import { ListGroupItem, Button, Collapse } from 'react-bootstrap';
 
-const NavItem = ({
-  children,
-  href,
-  depth,
-  title,
-  info: Info,
-  open: openProp,
-  icon: Icon,
-  className,
-}) => {
-  const [open, setOpen] = useState(openProp);
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  let paddingLeft = 8;
-
-  if (depth > 0) {
-    paddingLeft = 32 + 8 * depth;
-  }
-
-  const style = { paddingLeft };
-
-  if (children) {
-    return (
-      <ListGroupItem style={classes.item} className='borderless' key={title}>
-        <Button
-          onClick={handleToggle}
-          style={classes.button}
-          className='d-flex align-items-center py-2 unfocus'
-          variant='link'
-        >
-          {Icon && <Icon style={classes.icon} size='20' />}
-          <span style={classes.title}>{title}</span>
-          {open ? (
-            <i className='fas fa-chevron-up' />
-          ) : (
-            <i className='fas fa-chevron-down' />
-          )}
-        </Button>
-        <Collapse in={open} className='borderless ml-2' style={style}>
-          {children}
-        </Collapse>
-      </ListGroupItem>
-    );
-  }
-
-  return (
-    <ListGroupItem style={classes.itemLeaf} key={title} className='borderless'>
-      <NavLink
-        exact
-        to={href}
-        className='d-flex align-items-center py-2 sidenavItem'
-      >
-        {Icon && <Icon style={classes.icon} size='20' />}
-        <span style={classes.title}>{title}</span>
-        {Info && <Info />}
-      </NavLink>
-    </ListGroupItem>
-  );
-};
-
-const classes = {
+const useStyles = makeStyles((theme) => ({
   button: {
     color: '#333',
     padding: '10px 0px',
@@ -105,6 +44,74 @@ const classes = {
   title: {
     marginRight: 'auto',
   },
+}));
+
+const NavItem = ({
+  children,
+  href,
+  depth,
+  title,
+  info: Info,
+  open: openProp,
+  icon,
+  className,
+}) => {
+  const [open, setOpen] = useState(openProp);
+  const classes = useStyles();
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  let paddingLeft = 8;
+
+  if (depth > 0) {
+    paddingLeft = 32 + 8 * depth;
+  }
+
+  const style = { paddingLeft };
+
+  if (children) {
+    return (
+      <ListGroupItem className={`borderless ${clsx(classes.item)}`} key={title}>
+        <Button
+          onClick={handleToggle}
+          className={`d-flex align-items-center py-2 unfocus ${clsx(
+            classes.button
+          )}`}
+          variant='link'
+        >
+          {icon && <Icon className={classes.icon} icon={icon} size='20' />}
+          <span className={classes.title}>{title}</span>
+          {open ? (
+            <i className='fas fa-chevron-up' />
+          ) : (
+            <i className='fas fa-chevron-down' />
+          )}
+        </Button>
+        <Collapse in={open} className='borderless ml-2' style={style}>
+          {children}
+        </Collapse>
+      </ListGroupItem>
+    );
+  }
+
+  return (
+    <ListGroupItem
+      className={`borderless ${clsx(classes.itemLeaf)}`}
+      key={title}
+    >
+      <NavLink
+        exact
+        to={href}
+        className='d-flex align-items-center py-2 sidenavItem'
+      >
+        {icon && <Icon className={classes.icon} icon={icon} size='20' />}
+        <span className={classes.title}>{title}</span>
+        {Info && <Info />}
+      </NavLink>
+    </ListGroupItem>
+  );
 };
 
 NavItem.propTypes = {};
