079dedb3cf5ca4dec60a8642705bbe17619d84c1
sadewole <samueladewole15@gmail.com>
2020-10-15 18:14:23 +0100

    header and billing tab

--- src/pages/doctorBoard/Patient/PatientDetails/Patials/Billing.js
+++ src/pages/doctorBoard/Patient/PatientDetails/Patials/Billing.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Billings/Header';
+import {
+  Box,
+  Container,
+  Divider,
+  Tab,
+  Tabs,
+  makeStyles,
+} from '@material-ui/core';
+import General from './Billings/General';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+}));
 
 const Billing = () => {
+  const [currentTab, setCurrentTab] = useState('general');
+
+  const tabs = [
+    { value: 'general', label: 'General' },
+    { value: 'prepaid', label: 'Prepaid' },
+    { value: 'transaction', label: 'Transaction' },
+  ];
+
+  const handleTabsChange = (event, value) => {
+    setCurrentTab(value);
+  };
+
   return (
-    <div>
+    <Container maxWidth='lg'>
       <Header />
-    </div>
+      <Box mt={3}>
+        <Tabs
+          onChange={handleTabsChange}
+          scrollButtons='auto'
+          value={currentTab}
+          variant='scrollable'
+          indicatorColor='primary'
+          textColor='primary'
+        >
+          {tabs.map((tab) => (
+            <Tab key={tab.value} label={tab.label} value={tab.value} />
+          ))}
+        </Tabs>
+      </Box>
+      <Divider />
+      <Box mt={3}>{currentTab === 'general' && <General />}</Box>
+    </Container>
   );
 };
 
--- src/pages/doctorBoard/Patient/PatientDetails/Patials/Billings/Header.js
+++ src/pages/doctorBoard/Patient/PatientDetails/Patials/Billings/Header.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles({
     color: '#000 !important',
   },
   color3: {
-    background: colors.red['600'],
+    background: colors.green['600'],
   },
   currency: {
     display: 'block',
@@ -36,53 +36,63 @@ const Header = (props) => {
   return (
     <div className={classes.root}>
       <Row>
-        <Col item md={8} xs={8}>
-          <Card
-            color='primary'
-            className={`d-flex justify-content-between info-box text-white ${classes.color1}`}
-          >
-            <span className='info-box-icon'>
-              <Icon icon={GemIcon} />
-            </span>
-            <div className='flex-grow-1'>
-              <span className={`d-block ${classes.bText}`}>Total Balance</span>
-              <span className={classes.currency}>
-                {currencyFormatter(56000)}
-              </span>
-            </div>
-          </Card>
-          <div className='d-flex'>
-            <Card
-              color='primary'
-              className={`d-flex justify-content-between info-box text-white mr-1 ${classes.color2}`}
-            >
-              <span className='info-box-icon'>
-                <Icon icon={CoinsIcon} />
-              </span>
-              <div className='flex-grow-1'>
-                <span className={`d-block ${classes.bText}`}>Debit</span>
-                <span className={classes.currency}>
-                  {currencyFormatter(5000)}
+        <Col item md={12} xs={8}>
+          <Row>
+            <Col lg='4' md='12'>
+              <Card
+                color='primary'
+                className={`d-flex justify-content-between info-box text-white ${classes.color1}`}
+              >
+                <span className='info-box-icon'>
+                  <Icon icon={GemIcon} />
                 </span>
-              </div>
-            </Card>
-            <Card
-              color='primary'
-              className={`d-flex justify-content-between info-box text-white ${classes.color3}`}
-            >
-              <span className='info-box-icon'>
-                <Icon icon={DollarIcon} />
-              </span>
-              <div className='flex-grow-1'>
-                <span className={`d-block ${classes.bText}`}>Credit</span>
-                <span className={classes.currency}>
-                  {currencyFormatter(6000)}
+                <div className='flex-grow-1'>
+                  <span className={`d-block ${classes.bText}`}>
+                    Total Balance
+                  </span>
+                  <span className={classes.currency}>
+                    {currencyFormatter(56000)}
+                  </span>
+                </div>
+              </Card>
+            </Col>
+            <Col lg='4' md='6'>
+              {' '}
+              <Card
+                color='primary'
+                className={`d-flex justify-content-between info-box text-white mr-2 ${classes.color2}`}
+              >
+                <span className='info-box-icon'>
+                  <Icon icon={CoinsIcon} />
                 </span>
-              </div>
-            </Card>
-          </div>
+                <div className='flex-grow-1'>
+                  <span className={`d-block ${classes.bText}`}>Debit</span>
+                  <span className={classes.currency}>
+                    {currencyFormatter(5000)}
+                  </span>
+                </div>
+              </Card>
+            </Col>
+            <Col lg='4' md='6'>
+              {' '}
+              <Card
+                color='primary'
+                className={`d-flex justify-content-between info-box text-white ${classes.color3}`}
+              >
+                <span className='info-box-icon'>
+                  <Icon icon={DollarIcon} />
+                </span>
+                <div className='flex-grow-1'>
+                  <span className={`d-block ${classes.bText}`}>Credit</span>
+                  <span className={classes.currency}>
+                    {currencyFormatter(6000)}
+                  </span>
+                </div>
+              </Card>
+            </Col>
+          </Row>
         </Col>
-        <Col item md={4} xs={4} className='text-right'>
+        <Col item md={4} xs={4} className='text-'>
           <Button variant='outline-secondary'>
             <InlineIcon icon={PlusCircleIcon} className='mr-1' />
             Add Transaction
