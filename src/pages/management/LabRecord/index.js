d26ca144fbfa3015e3e01477ce501c1fbf417c9c
Teniola <fatunmbiteniola001@gmail.com>
2021-07-23 05:52:04 +0100

    initial commit for lab record

--- src/pages/management/LabRecord/index.js
+++ src/pages/management/LabRecord/index.js
@@ -1,7 +1,77 @@
-import React from 'react';
+// import React, { useEffect } from 'react';
+// import { Container, Card } from 'react-bootstrap';
+// import Page from 'src/components/Page';
+// import { useDispatch, useSelector } from 'src/store';
+// import { fetchLabRecords } from 'src/slices/patient';
+// import { useParams } from 'react-router-dom';
+// import DoctorListView from "src/pages/management/Patient/PatientList";
+
+// const History = () => {
+//   const dispatch = useDispatch();
+//   const { allRecords } = useSelector((state) => state.patient);
+
+//   const { patientId } = useParams();
+//   useEffect(() => {
+//     dispatch(fetchLabRecords())
+//   }, [dispatch, patientId]);
+//   return (
+//     <Page
+//     style={{
+//       minHeight: '100%',
+//       paddingTop: '1em',
+//       paddingBottom: '3em',
+//     }}
+//     title='Patients List'
+//   >
+//     <Container fluid>
+//       <Card className='mt-3' style={{ borderRadius: '1rem' }}>
+//         <DoctorListView />
+//         {
+//           console.log(allRecords)
+//         }
+//       </Card>
+//     </Container>
+//   </Page>
+//           );
+// };
+
+// export default History;
+import React, { useEffect } from 'react';
+import { Container, Card } from 'react-bootstrap';
+import Page from 'src/components/Page';
+import Results from './Results';
+import { useDispatch, useSelector } from 'src/store';
+import { fetchAllPatient } from 'src/slices/patient';
+import { useParams } from "react-router-dom";
 
 const History = () => {
-  return <div>History Page</div>;
+  const dispatch = useDispatch();
+  const { allPatient, allRecords } = useSelector((state) => state.patient);
+  
+  const { patientId } = useParams();
+  useEffect(() => {
+    dispatch(fetchAllPatient());
+  }, [dispatch, patientId]);
+
+  return (
+    <Page
+      style={{
+        minHeight: '100%',
+        paddingTop: '1em',
+        paddingBottom: '3em',
+      }}
+      title='Patients List'
+    >
+      <Container fluid>
+        <Card className='mt-3' style={{ borderRadius: '1rem' }}>
+          <Results patients={allPatient} />
+          {
+            console.log(allRecords)
+          }
+        </Card>
+      </Container>
+    </Page>
+  );
 };
 
 export default History;
