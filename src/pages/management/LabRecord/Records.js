d3f8c1eb2b4e7fa9a00a20c439b94634e2b8e54a
Teniola <fatunmbiteniola001@gmail.com>
2021-07-25 19:11:52 +0100

    added setup to Records display page

--- src/pages/management/LabRecord/Records.js
+++ src/pages/management/LabRecord/Records.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { Link as RouterLink, useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Card, FormControl, Dropdown, ButtonGroup } from 'react-bootstrap';
+import { Tabs, Tab, Paper, TableRow, TableCell } from '@material-ui/core';
+import moment from 'moment';
+import {
+  Search as SearchIcon,
+  MoreHorizontal as MoreHorizontalIcon,
+} from 'react-feather';
+import Table from 'src/components/CustomTable';
+import { fetchLabRecords } from 'src/slices/patient';
+import { useDispatch, useSelector } from 'src/store';
 
-const Records = () => {
+
+const Records = ({ className, records }) => {
+    const dispatch = useDispatch();
+    const { patientId } = useParams();
+    const { labRecord } = useSelector((state) => state.patient);
+
+    useEffect(() => {
+      dispatch(fetchLabRecords(patientId));
+    }, [dispatch, patientId]);
+
+    console.log(labRecord, patientId);
     return (
-        <div>
-            <h1>Record List</h1>
-        </div>
+        <div>Record List</div>
     )
 }
+
+Records.propTypes = {
+  className: PropTypes.string,
+  records: PropTypes.array.isRequired,
+};
+
+Records.defaultProps = {
+  records: [],
+};
 
 export default Records;
