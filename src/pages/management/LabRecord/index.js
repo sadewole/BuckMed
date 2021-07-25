159f343c371e7c3330961e4c51275d2f3812ba3a
Teniola <fatunmbiteniola001@gmail.com>
2021-07-25 19:12:32 +0100

    finished up index page

--- src/pages/management/LabRecord/index.js
+++ src/pages/management/LabRecord/index.js
@@ -3,17 +3,15 @@ import { Container, Card } from 'react-bootstrap';
 import Page from 'src/components/Page';
 import Results from './Results';
 import { useDispatch, useSelector } from 'src/store';
-import { fetchAllPatient } from 'src/slices/patient';
-import { useParams } from "react-router-dom";
+import { fetchAllPatient} from 'src/slices/patient';
 
 const History = () => {
   const dispatch = useDispatch();
-  const { allPatient, allRecords } = useSelector((state) => state.patient);
+  const { allPatient } = useSelector((state) => state.patient);
   
-  const { patientId } = useParams();
   useEffect(() => {
     dispatch(fetchAllPatient());
-  }, [dispatch, patientId]);
+  }, [dispatch]);
 
   return (
     <Page
@@ -27,9 +25,6 @@ const History = () => {
       <Container fluid>
         <Card className='mt-3' style={{ borderRadius: '1rem' }}>
           <Results patients={allPatient} />
-          {
-            console.log(allRecords)
-          }
         </Card>
       </Container>
     </Page>
